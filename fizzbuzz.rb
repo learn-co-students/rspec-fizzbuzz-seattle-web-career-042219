a557e1b18d8e3afcf0ea369f871df9d014468708
Sean Watters <sean@h2os.org>
2019-04-09 05:33:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
+
+def fizzbuzz(num)
+
+  if num % 5 == 0 && num % 3 == 0
+    return "FizzBuzz"
+
+  elsif num % 5 == 0
+    return "Buzz"
+
+  elsif num % 3 == 0
+    return "Fizz"
+    
+  end
+end
 # See README.md for instructions on how to do this
